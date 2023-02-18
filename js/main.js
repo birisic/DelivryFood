26e14e618e94b61ce27eeac7bd603534a85202cf
kanibalkorps <birisicmartin02@gmail.com>
2023-02-18 12:44:11 +0100

    js testing

--- js/main.js
+++ js/main.js
@@ -10,8 +10,7 @@ if (!localStorage.getItem("navigation.json")) {
 //LOAD PAGE
 $(document).ready(function(){
     console.log(window.location.pathname);
-    console.log("loggg");
-    if (window.location.pathname == "/index.html" || window.location.pathname == "/") {
+    if (window.location.pathname == "/delivry/index.html" || window.location.pathname == "/delivry/") {
         //OWL CAROUSEL FOOD CATEGORIES
         foodCategoriesOwlCarouselPrint();
         $("#owl-example").owlCarousel();
