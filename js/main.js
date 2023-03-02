0c9cf0c2b7848f2e02d6579593d44f610002bb70
kanibalkorps <birisicmartin02@gmail.com>
2023-03-02 14:00:07 +0100

    log

--- js/main.js
+++ js/main.js
@@ -20,10 +20,11 @@ let restaurants = JSON.parse(localStorage.getItem("restaurants.json"));
 
 //LOAD PAGE
 $(document).ready(function(){
+    console.log(window.location.pathname);
     console.log(restaurants);
     //CREATE NAVBAR
     createNavbar();
-
+    
     //NAVBAR SHRINK AND INCREASE OPACITY ON SCROLL
     navbarShrinkOnScroll();
     
