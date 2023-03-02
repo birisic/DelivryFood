7d5702a4418cdb66c7bfa84ffb9de540a5691041
kanibalkorps <birisicmartin02@gmail.com>
2023-03-02 13:53:44 +0100

    dynamically print restaurants

--- js/main.js
+++ js/main.js
@@ -5,41 +5,104 @@ const BASE_DATA = "data/";
 
 //GET JSON DATA AND STORE IT IN LOCALSTORAGE BEFORE FULL PAGE LOADS
 if (!localStorage.getItem("categories-food.json")) {
-    ajaxCallback("categories-food.json", saveToLocalStorage)
+    ajaxCallback("categories-food.json", saveToLocalStorage);
 }
 if (!localStorage.getItem("navigation.json")) {
-    ajaxCallback("navigation.json", saveToLocalStorage)
+    ajaxCallback("navigation.json", saveToLocalStorage);
 }
-//GET RESTAURANTS.JSON !!
+if (!localStorage.getItem("navigation.json")) {
+    ajaxCallback("restaurants.json", saveToLocalStorage);
+}
 
 //SAVE CATEGORIES TO AN ARRAY GLOBALLY
 let foodCategories = JSON.parse(localStorage.getItem("categories-food.json"));
-
+let restaurants = JSON.parse(localStorage.getItem("restaurants.json"));
 
 //LOAD PAGE
 $(document).ready(function(){
-    if (window.location.pathname == "/delivry/index.html" || window.location.pathname == "/delivry/") {//include repository name
-        //OWL CAROUSEL FOOD CATEGORIES
-        foodCategoriesOwlCarouselPrint();
-        $("#owl-example").owlCarousel();
-    }
-    
-
+    console.log(restaurants);
     //CREATE NAVBAR
     createNavbar();
 
     //NAVBAR SHRINK AND INCREASE OPACITY ON SCROLL
     navbarShrinkOnScroll();
+    
+    //INDEX PAGE
+    if (window.location.pathname == "/delivry/index.html" || window.location.pathname == "/delivry/") {//include repository name
+        //OWL CAROUSEL FOOD CATEGORIES
+        foodCategoriesOwlCarouselPrint();
+        $("#owl-example").owlCarousel();
 
 
-    //READ MORE/LESS ABOUT SECTION
-    readMoreOrLess();
+        //READ MORE/LESS ABOUT SECTION
+        readMoreOrLess();
+    }
+    
+    //RESTAURANTS PAGE
+    if (window.location.pathname == "/delivry/restaurants.html") {
+        //PRINT DEFAULT OR FILTERED RESTAURANTS
+        printRestaurants();
+    }
+
+    
+
+
+    
 
 
     //CREATE CATEGORY FILTERS
     createCategoryFilters();
     showClickedFilters();
 })
+
+
+function printRestaurants() {
+    let restaurantsRow = document.getElementById("mb-restaurants");
+    let print = "";
+
+    restaurants.forEach(restaurant => {
+        print += `<div class="col-lg-4 col-md-6 col-12 mb-5 d-flex justify-content-center align-items-center">
+        <a href="index.html" class="mb-restaurant-a">
+           <div class="card border-0 rounded">
+              <img src="${BASE_IMG}${restaurant.image.alt}${restaurant.image.src}" alt="${restaurant.image.alt}" class="card-img mb-restaurant-img"/>
+              <div class="card-body p-0 mb-restaurant-card position-absolute rounded">
+                 <span class="mb-restaurant-delivery rounded-bottom">${restaurant.deliveryTime}min <i class="fa-regular fa-clock"></i></span>
+                    <div class="row w-100 m-0 mb-restaurant-bottom d-flex justify-content-between align-items-end">
+                       <div class="mb-restaurant-title col-md-7 col-6">
+                          <span class="mb-restaurant-tag text-white fw-bold pb-0 ps-1">${restaurant.tag}</span>
+                          <p class="mb-restaurant-text card-text text-white pb-3 ps-1 pb-xl-3 fw-bold">${restaurant.name}</p>
+                       </div>
+                       <div class="mb-restaurant-spans col-md-5 col-6 d-flex justify-content-end align-items-center py-3 px-1">
+                          <span class="bg-warning p-1 rounded"><i class="fa-solid fa-bicycle"></i>${printDeliveryPrice(restaurant.delivery)}</span>
+                          <span class="text-capitalize"><i class="fa-solid fa-star"></i>(${restaurant.recommendations})</span>
+                       </div>
+                    </div>
+                 
+              </div>
+           </div>
+        </a>
+     </div>`
+    });
+
+    restaurantsRow.innerHTML = print;
+
+}
+
+
+function printDeliveryPrice(arr) {
+    let print = "";
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].type == "free") {
+            print = "FREE";
+            break;
+        }
+        else {
+            print += arr[i].price + "RSD";
+            break;
+        }
+    }
+    return print;
+}
 
 
 function ajaxCallback(file,callback) {
